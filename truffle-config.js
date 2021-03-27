55725108cf9e0ad643503d7e78d84c783fc5eaa7
xiangzhengfeng <eoszhengfeng@163.com>
2021-03-27 14:38:39 +0800

    人生何曾驻旧足，前端处处是新旅

--- truffle-config.js
+++ truffle-config.js
@@ -1,8 +1,8 @@
 const path = require("path");
+const config = require("./config")
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
     develop: {
@@ -12,6 +12,14 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*"
-    }
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(config.mnemonic, config.infuraUrl),
+      network_id: 3,
+      gas: 5500000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
   }
 };
